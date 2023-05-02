34709cc47f2b3d17a0ed121b248ee4c174a04fb9
JAMES <jameskibunja17@gmail.com>
2023-05-02 12:33:42 +0300

    Done Assignment

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,7 +2,16 @@ class StudentsController < ApplicationController
 
   def index
     students = Student.all
+    if params[:name]
+      students = students.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    end
     render json: students
+  end
+  
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
